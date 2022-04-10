119ba3bc8c12a1a0063a28c27e1e3f24b6946b7e
dmax1 <2871466630@qq.com>
2022-04-10 22:26:57 +0800

    first commit

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -1,7 +1,51 @@
+/*
+ * @Author: DragonMax
+ * @Date: 2022-04-10 
+ * @LastEditTime: 2022-04-10 
+ * @LastEditors: DragonMax
+ * @Description: 
+ * @FilePath: \typescript\fetchjson\index.ts
+ */
 import axios from "axios"
 
 const url = "https://jsonplaceholder.typicode.com/users/1"
+
+
+interface User {
+    id: number,
+    name: string,
+    username: string,
+    email: string,
+    phone: string,
+    website: string,
+    company: Company
+}
+
+interface Company {
+    name: string,
+    catchPhrase: string,
+    bs: string
+}
 axios.get(url)
     .then(res => {
-        console.log(res.data);
+        const user = res.data as User;
+
+        logUserInfo(user);
+       
     })
+    
+const  logUserInfo = (user: User): void =>
+{
+    const {id, name, username, email, phone, website, company}: User = user
+    console.log(`
+        His id is ${id},
+        His name is ${name},
+        His username is ${username}
+        His username is ${email}
+        His phone is ${phone},
+        His website is ${website},
+        His company's name is ${company.name},
+        His company's catchPhrase is ${company.catchPhrase},
+        His company's bs is ${company.bs}`
+    )
+}
