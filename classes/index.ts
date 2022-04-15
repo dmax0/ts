5eab24cdd2e786e633c451bc04317269f784de5d
dmax1 <2871466630@qq.com>
2022-04-15 18:11:16 +0800

    add classes

---
+++ classes/index.ts
@@ -0,0 +1,8 @@
+/*
+ * @Author: DragonMax
+ * @Date: 2022-04-15 
+ * @LastEditTime: 2022-04-15 
+ * @LastEditors: DragonMax
+ * @Description: classes
+ * @FilePath: \typescript\classes\index.ts
+ */
--- interfaces/index.ts
+++ interfaces/index.ts
@@ -13,16 +13,27 @@ interface Vehicle {
     Broken: boolean;
     summary: () => string;
 }
-
+interface Reportable {
+    summary(): string;
+}
 const logVehicleInfo = (vehicle: Vehicle): void  => {
     const info = `Name is ${vehicle.name},created at ${vehicle.year}.Broken ? ${vehicle.Broken}`
     console.log(info)
 } 
+const printSummary = (reportable: Reportable) => {
+    console.log(reportable.summary())
+}
+const myDrink  = {
+    name: 'orange',
+    carbonated: false,
+    suger: 40, 
+    summary: (): string => {return `Name is ${myDrink.name}`}
+}
 const myCar: Vehicle = {
     name: 'Agera RS',
     year: 2017,
     Broken: false,
     summary:  (): string => { return `Name is ${myCar.name}`}
 }
-logVehicleInfo(myCar)
-console.log(myCar.summary())
+printSummary(myCar)
+printSummary(myDrink)
